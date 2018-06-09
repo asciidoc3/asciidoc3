72a3e8a29f51ae0ecfcb8bc8896578f06b49bde5
asciidoc3 <34128292+asciidoc3@users.noreply.github.com>
2018-06-09 22:10:32 +0200

    Update testasciidoc3.py to AsciiDoc3/Python3
    asciidoc3 (Berthold Gehrke berthold.gehrke@gmail.com)

--- tests/testasciidoc3.py
+++ tests/testasciidoc3.py
@@ -1,8 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-USAGE = '''Usage: testasciidoc.py [OPTIONS] COMMAND
+"""
+Provides a test series. Many features of AsciiDoc3 are examined.
+Use it together with testasciidoc3.conf and asciidoc3api.py 
+living in the same directory.
+(c) 2018 Berthold Gehrke. Free use is granted under the
+GNU General Public License v3 or higher
+http://www.gnu.org/licenses/gpl-3.0.en.html
+"""
 
-Run AsciiDoc conformance tests specified in configuration FILE.
+import difflib
+import getopt
+import io
+import os
+import re
+import sys
+
+import asciidoc3api
+
+USAGE = '''Usage: testasciidoc3.py [OPTIONS] COMMAND
+Run AsciiDoc3 conformance tests specified in configuration FILE.
 
 Commands:
   list                          List tests
@@ -12,29 +29,18 @@ Commands:
 Options:
   -f, --conf-file=CONF_FILE
         Use configuration file CONF_FILE (default configuration file is
-        testasciidoc.conf in testasciidoc.py directory)
+        testasciidoc3.conf in testasciidoc3.py directory)
   --force
         Update all test data overwriting existing data'''
 
 
-__version__ = '0.1.1'
-__copyright__ = 'Copyright (C) 2009 Stuart Rackham'
+__version__ = '3.0.1'          # '3' stands for Python3
+__copyright__ = 'Copyright (C) 2009 Stuart Rackham, (c) 2018 Berthold Gehrke'
 
 
-import os, sys, re, difflib
-
-if sys.platform[:4] == 'java':
-    # Jython cStringIO is more compatible with CPython StringIO.
-    import cStringIO as StringIO
-else:
-    import StringIO
-
-import asciidocapi
-
-
-BACKENDS = ('html4','xhtml11','docbook','html5')    # Default backends.
+BACKENDS = ('html4', 'xhtml11', 'docbook', 'html5')    # Default backends.
 BACKEND_EXT = {'html4':'.html', 'xhtml11':'.html', 'docbook':'.xml',
-        'slidy':'.html','html5':'.html'}
+               'slidy':'.html', 'html5':'.html'}
 
 
 def iif(condition, iftrue, iffalse=None):
@@ -44,9 +50,9 @@ def iif(condition, iftrue, iffalse=None):
     False value defaults to 0 if the true value is a number.
     """
     if iffalse is None:
-        if isinstance(iftrue, basestring):
+        if isinstance(iftrue, str):
             iffalse = ''
-        if type(iftrue) in (int, float):
+        if isinstance(iftrue, (int, float)):
             iffalse = 0
     if condition:
         return iftrue
@@ -54,13 +60,16 @@ def iif(condition, iftrue, iffalse=None):
         return iffalse
 
 def message(msg=''):
-    print >>sys.stderr, msg
+    """
+    Output is written (only) to stderr.
+    """
+    print(msg, file=sys.stderr)
 
 def strip_end(lines):
     """
     Strip blank strings from the end of list of strings.
     """
-    for i in range(len(lines)-1,-1,-1):
+    for i in range(len(lines)-1, -1, -1):
         if not lines[i]:
             del lines[i]
         else:
@@ -70,19 +79,22 @@ def normalize_data(lines):
     """
     Strip comments and trailing blank strings from lines.
     """
-    result = [ s for s in lines if not s.startswith('#') ]
+    result = [s for s in lines if not s.startswith('#')]
     strip_end(result)
     return result
 
 
-class AsciiDocTest(object):
+class AsciiDoc3Test(object):
+    """
+    Serves as 'default' class, if no other configuration is given.
+    """
 
     def __init__(self):
         self.number = None      # Test number (1..).
         self.name = ''          # Optional test name.
         self.title = ''         # Optional test name.
         self.description = []   # List of lines followoing title.
-        self.source = None      # AsciiDoc test source file name.
+        self.source = None      # AsciiDoc3 test source file name.
         self.options = []
         self.attributes = {}
         self.backends = BACKENDS
@@ -95,9 +107,9 @@ class AsciiDocTest(object):
         the test name and output file type.
         """
         return '%s-%s%s' % (
-                os.path.normpath(os.path.join(self.datadir, self.name)),
-                backend,
-                BACKEND_EXT[backend])
+            os.path.normpath(os.path.join(self.datadir, self.name)),
+            backend,
+            BACKEND_EXT[backend])
 
     def parse(self, lines, confdir, datadir):
         """
@@ -120,18 +132,18 @@ class AsciiDocTest(object):
                     continue
                 reo = re.match(r'^%\s*(?P<directive>[\w_-]+)', l[0])
                 if not reo:
-                    raise (ValueError, 'illegal directive: %s' % l[0])
+                    raise ValueError
                 directive = reo.groupdict()['directive']
                 data = normalize_data(l[1:])
                 if directive == 'source':
                     if data:
                         self.source = os.path.normpath(os.path.join(
-                                self.confdir, os.path.normpath(data[0])))
+                            self.confdir, os.path.normpath(data[0])))
                 elif directive == 'options':
                     self.options = eval(' '.join(data))
-                    for i,v in enumerate(self.options):
-                        if isinstance(v, basestring):
-                            self.options[i] = (v,None)
+                    for i, v in enumerate(self.options):
+                        if isinstance(v, str):
+                            self.options[i] = (v, None)
                 elif directive == 'attributes':
                     self.attributes = eval(' '.join(data))
                 elif directive == 'backends':
@@ -139,7 +151,7 @@ class AsciiDocTest(object):
                 elif directive == 'name':
                     self.name = data[0].strip()
                 else:
-                    raise (ValueError, 'illegal directive: %s' % l[0])
+                    raise ValueError
         if not self.title:
             self.title = self.source
         if not self.name:
@@ -156,8 +168,8 @@ class AsciiDocTest(object):
         Returns True if the output test data file is missing or out of date.
         """
         return self.is_missing(backend) or (
-               os.path.getmtime(self.source)
-               > os.path.getmtime(self.backend_filename(backend)))
+            os.path.getmtime(self.source)
+            > os.path.getmtime(self.backend_filename(backend)))
 
     def get_expected(self, backend):
         """
@@ -167,7 +179,7 @@ class AsciiDocTest(object):
         try:
             result = f.readlines()
             # Strip line terminators.
-            result = [ s.rstrip() for s in result ]
+            result = [s.rstrip() for s in result]
         finally:
             f.close()
         return result
@@ -176,12 +188,12 @@ class AsciiDocTest(object):
         """
         Generate and return test data output for backend.
         """
-        asciidoc = asciidocapi.AsciiDocAPI()
-        asciidoc.options.values = self.options
-        asciidoc.attributes = self.attributes
+        asciidoc3 = asciidoc3api.AsciiDoc3API()
+        asciidoc3.options.values = self.options
+        asciidoc3.attributes = self.attributes
         infile = self.source
-        outfile = StringIO.StringIO()
-        asciidoc.execute(infile, outfile, backend)
+        outfile = io.StringIO()
+        asciidoc3.execute(infile, outfile, backend)
         return outfile.getvalue().splitlines()
 
     def update_expected(self, backend):
@@ -190,12 +202,12 @@ class AsciiDocTest(object):
         """
         lines = self.generate_expected(backend)
         if not os.path.isdir(self.datadir):
-            print('CREATING: %s' % self.datadir)
+            print(('CREATING: %s' % self.datadir))
             os.mkdir(self.datadir)
-        f = open(self.backend_filename(backend),'w+')
+        f = open(self.backend_filename(backend), 'w+')
         try:
-            print('WRITING: %s' % f.name)
-            f.writelines([ s + os.linesep for s in lines])
+            print(('WRITING: %s' % f.name))
+            f.writelines([s + os.linesep for s in lines])
         finally:
             f.close()
 
@@ -222,9 +234,9 @@ class AsciiDocTest(object):
             backends = [backend]
         result = True   # Assume success.
         self.passed = self.failed = self.skipped = 0
-        print('%d: %s' % (self.number, self.title))
+        print(('%d: %s' % (self.number, self.title)))
         if self.source and os.path.isfile(self.source):
-            print('SOURCE: asciidoc: %s' % self.source)
+            print(('SOURCE: asciidoc3: %s' % self.source))
             for backend in backends:
                 fromfile = self.backend_filename(backend)
                 if not self.is_missing(backend):
@@ -237,9 +249,9 @@ class AsciiDocTest(object):
                         lines.append(line)
                     if lines:
                         result = False
-                        self.failed +=1
+                        self.failed += 1
                         lines = lines[3:]
-                        print('FAILED: %s: %s' % (backend, fromfile))
+                        print(('FAILED: %s: %s' % (backend, fromfile)))
                         message('+++ %s' % fromfile)
                         message('--- got')
                         for line in lines:
@@ -247,22 +259,25 @@ class AsciiDocTest(object):
                         message()
                     else:
                         self.passed += 1
-                        print('PASSED: %s: %s' % (backend, fromfile))
+                        print(('PASSED: %s: %s' % (backend, fromfile)))
                 else:
                     self.skipped += 1
-                    print('SKIPPED: %s: %s' % (backend, fromfile))
+                    print(('SKIPPED: %s: %s' % (backend, fromfile)))
         else:
             self.skipped += len(backends)
             if self.source:
                 msg = 'MISSING: %s' % self.source
             else:
-                msg = 'NO ASCIIDOC SOURCE FILE SPECIFIED'
+                msg = 'NO ASCIIDOC3 SOURCE FILE SPECIFIED'
             print(msg)
         print('')
         return result
 
 
-class AsciiDocTests(object):
+class AsciiDoc3Tests(object):
+    """
+    Containing functions which 'do the work' of testing.
+    """
 
     def __init__(self, conffile):
         """
@@ -272,7 +287,7 @@ class AsciiDocTests(object):
         # All file names are relative to configuration file directory.
         self.confdir = os.path.dirname(self.conffile)
         self.datadir = self.confdir # Default expected files directory.
-        self.tests = []             # List of parsed AsciiDocTest objects.
+        self.tests = []             # List of parsed AsciiDoc3Test objects.
         self.globals = {}
         f = open(self.conffile)
         try:
@@ -282,18 +297,18 @@ class AsciiDocTests(object):
         first = True
         while not lines.eol():
             s = lines.read_until(r'^%+$')
-            s = [ l for l in s if l]    # Drop blank lines.
+            s = [l for l in s if l]    # Drop blank lines.
             # Must be at least one non-blank line in addition to delimiter.
             if len(s) > 1:
                 # Optional globals precede all tests.
-                if first and re.match(r'^%\s*globals$',s[0]):
+                if first and re.match(r'^%\s*globals$', s[0]):
                     self.globals = eval(' '.join(normalize_data(s[1:])))
                     if 'datadir' in self.globals:
                         self.datadir = os.path.join(
-                                self.confdir,
-                                os.path.normpath(self.globals['datadir']))
+                            self.confdir,
+                            os.path.normpath(self.globals['datadir']))
                 else:
-                    test = AsciiDocTest()
+                    test = AsciiDoc3Test()
                     test.parse(s[1:], self.confdir, self.datadir)
                     self.tests.append(test)
                     test.number = len(self.tests)
@@ -306,21 +321,23 @@ class AsciiDocTests(object):
         """
         self.passed = self.failed = self.skipped = 0
         for test in self.tests:
-            if (not test.disabled or number) and (not number or number == test.number) and (not backend or backend in test.backends):
+            if (not test.disabled or number) and \
+               (not number or number == test.number) and \
+               (not backend or backend in test.backends):
                 test.run(backend)
                 self.passed += test.passed
                 self.failed += test.failed
                 self.skipped += test.skipped
         if self.passed > 0:
-            print('TOTAL PASSED:  %s' % self.passed)
+            print(('TOTAL PASSED:  %s' % self.passed))
         if self.failed > 0:
-            print('TOTAL FAILED:  %s' % self.failed)
+            print(('TOTAL FAILED:  %s' % self.failed))
         if self.skipped > 0:
-            print('TOTAL SKIPPED: %s' % self.skipped)
+            print(('TOTAL SKIPPED: %s' % self.skipped))
 
     def update(self, number=None, backend=None, force=False):
         """
-        Regenerate expected test data and update configuratio file.
+        Regenerate expected test data and update configuration file.
         """
         for test in self.tests:
             if (not test.disabled or number) and (not number or number == test.number):
@@ -331,7 +348,7 @@ class AsciiDocTests(object):
         Lists tests to stdout.
         """
         for test in self.tests:
-            print '%d: %s%s' % (test.number, iif(test.disabled,'!'), test.title)
+            print('%d: %s%s' % (test.number, iif(test.disabled, '!'), test.title))
 
 
 class Lines(list):
@@ -374,26 +391,26 @@ def usage(msg=None):
 
 if __name__ == '__main__':
     # Process command line options.
-    import getopt
+    #import getopt
     try:
-        opts,args = getopt.getopt(sys.argv[1:], 'f:', ['force'])
+        opts, args = getopt.getopt(sys.argv[1:], 'f:', ['force'])
     except getopt.GetoptError:
         usage('illegal command options')
         sys.exit(1)
     if len(args) == 0:
         usage()
         sys.exit(1)
-    conffile = os.path.join(os.path.dirname(sys.argv[0]), 'testasciidoc.conf')
+    conffile = os.path.join(os.path.dirname(sys.argv[0]), 'testasciidoc3.conf')
     force = False
-    for o,v in opts:
+    for o, v in opts:
         if o == '--force':
             force = True
-        if o in ('-f','--conf-file'):
+        if o in ('-f', '--conf-file'):
             conffile = v
     if not os.path.isfile(conffile):
         message('missing CONF_FILE: %s' % conffile)
         sys.exit(1)
-    tests = AsciiDocTests(conffile)
+    tests = AsciiDoc3Tests(conffile)
     cmd = args[0]
     number = None
     backend = None
@@ -405,7 +422,7 @@ if __name__ == '__main__':
     if backend and backend not in BACKENDS:
         message('illegal BACKEND: %s' % backend)
         sys.exit(1)
-    if number is not None and  number not in range(1, len(tests.tests)+1):
+    if number is not None and  number not in list(range(1, len(tests.tests)+1)):
         message('illegal test NUMBER: %d' % number)
         sys.exit(1)
     if cmd == 'run':
@@ -418,3 +435,4 @@ if __name__ == '__main__':
         tests.list()
     else:
         usage('illegal COMMAND: %s' % cmd)
+        
